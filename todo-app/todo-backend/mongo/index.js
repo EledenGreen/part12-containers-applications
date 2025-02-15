bbacc7d0adca1941e3ea9b5c9feba4c0b729fedf
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2025-02-16 02:58:27 +0530

    env added

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
+if (MONGO_URL && !mongoose.connection.readyState) {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log('connected to ', MONGO_URL)
+} else {
+  console.log(`not connected ${MONGO_URL}`)
+}
 
 module.exports = {
-  Todo
+  Todo,
 }
